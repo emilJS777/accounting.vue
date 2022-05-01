8b3a0f30b3997cdd0ee0686061d53c04cafb04e5
emil <e.pargevich@mail.ru>
2022-05-01 17:57:09 +0400

    asas

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    https: false,
+    port: 4002,
+    public: 'http://0.0.0.0:4002'
+  },
 })
